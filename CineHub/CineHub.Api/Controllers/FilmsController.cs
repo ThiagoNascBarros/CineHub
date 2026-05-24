d700f17cc190dd96d22c25b01fed274a5354a299
Thiago <thiago171nasciment@gmail.com>
2026-05-23 22:58:49 -0300

    CNH-001 Create project is API

---
+++ CineHub/CineHub.Api/Controllers/FilmsController.cs
@@ -0,0 +1,26 @@
+﻿using CineHub.Api.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CineHub.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FilmsController : ControllerBase
+    {
+        private List<Film> films = [];
+
+        [HttpGet]
+        public List<Film> Get()
+        {
+            return films;
+        }
+
+        [HttpPost]
+        public void Post([FromBody] Film film)
+        {
+            films.Add(film);
+        }
+
+    }
+}
