1f704d08bebe1b6db57d2752da2a6b994a60ded2
Thiago <thiago171nasciment@gmail.com>
2026-05-23 23:00:52 -0300

    Reapply "CNH-001 Create project is API"
    This reverts commit c54d2f61ed20ac757a3cd400f274f983d653a6e6.

---
+++ CineHub/CineHub.Api/Models/Film.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CineHub.Api.Models
+{
+    public class Film
+    {
+        [Required(ErrorMessage = "The title is mandatory")]
+        public string Title { get; set; }
+
+        [Required(ErrorMessage = "The genre is mandatory")]
+        [MaxLength(50, ErrorMessage = "The genre cannot exceed 50 characters.")]
+        public string Genre { get; set; }
+
+        [Required]
+        [Range(70, 600)]
+        public int DurationInMinutes { get; set; }
+    }
+}
